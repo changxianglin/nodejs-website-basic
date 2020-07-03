300be41532d1ce7220e96e0f38cce78235f181ec
changxianglin <281532612@qq.com>
2020-07-04 01:18:45 +0800

    post

--- http-test/app.js
+++ http-test/app.js
@@ -1,15 +1,31 @@
 const http = require('http')
 const querystring = require('querystring')
 
+// const server = http.createServer((req, res) => {
+//   console.log('method: ', req.method) // get
+//   const url = req.url
+//   console.log('url: ', url)
+//   req.query = querystring.parse(url.split('?')[1])
+//   console.log('query: ', req.query)
+//   res.end(
+//     JSON.stringify(req.query)
+//   )
+// })
+
 const server = http.createServer((req, res) => {
-  console.log('method: ', req.method) // get
-  const url = req.url
-  console.log('url: ', url)
-  req.query = querystring.parse(url.split('?')[1])
-  console.log('query: ', req.query)
-  res.end(
-    JSON.stringify(req.query)
-  )
+  if (req.method === 'POST') {
+    // req 数据格式
+    console.log('req content-type: ', req.headers['content-type'])
+    // 接收数据
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      console.log('postData: ', postData)
+      res.end('hello world ！')
+    })
+  }
 })
 
 server.listen(8080)
