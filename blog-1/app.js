2a52799551e4972fb39bc63af70f12cc399764c8
changxianglin <281532612@qq.com>
2020-07-05 11:45:29 +0800

    promise postdata

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,6 +2,31 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(JSON.parse(postData))
+    })
+  })
+  return promise
+}
+
 const serverHandle = (req, res) => {
   // 设置返回格式
   res.setHeader('Content-type', 'application/json')
@@ -13,29 +38,34 @@ const serverHandle = (req, res) => {
   // 解析 query
   req.query = querystring.parse(url.split('?')[1])
 
-  // 处理 blog 路由
-  const blogData = handleBlogRouter(req, res)
+  // 处理 post data
+  getPostData(req).then(postData => {
+    req.body = postData
 
-  if (blogData) {
-    res.end(
-      JSON.stringify(blogData)
-    )
-    return
-  }
+    // 处理 blog 路由
+    const blogData = handleBlogRouter(req, res)
 
-  // 处理 user 路由
-  const userData = handleUserRouter(req, res)
-  if (userData) {
-    res.end(
-      JSON.stringify(userData)
-    )
-    return
-  }
+    if (blogData) {
+      res.end(
+        JSON.stringify(blogData)
+      )
+      return
+    }
 
-  // 未命中路由, 返回 404 
-  res.writeHead(404, {'Content-type': 'text/plain'})
-  res.write('404 Not Found\n')
-  res.end()
+    // 处理 user 路由
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+      res.end(
+        JSON.stringify(userData)
+      )
+      return
+    }
+
+    // 未命中路由, 返回 404 
+    res.writeHead(404, {'Content-type': 'text/plain'})
+    res.write('404 Not Found\n')
+    res.end()
+  })
 }
 
 module.exports = serverHandle
