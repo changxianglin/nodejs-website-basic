bcff38ff6085ad7a6d9327d8e01537fd9386e653
changxianglin <281532612@qq.com>
2020-07-11 15:56:32 +0800

    cookie use login

--- blog-1/app.js
+++ blog-1/app.js
@@ -38,6 +38,21 @@ const serverHandle = (req, res) => {
   // 解析 query
   req.query = querystring.parse(url.split('?')[1])
 
+  // 解析 cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || '' // k1=v1;k2=v2;k3=v3
+  console.log('测试值', cookieStr == '', cookieStr.split(';'))
+  cookieStr.split(';').forEach(item => {
+    if (!item) {
+      return 
+    }
+    const arr = item.split('=')
+    const key = arr[0]
+    const val = arr[1]
+    req.cookie[key] = val
+  })
+  console.log('req.cookie is ', req.cookie)
+
   // 处理 post data
   getPostData(req).then(postData => {
     req.body = postData
