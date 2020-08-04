7a81d71eb15d0347467475c7e42f68f2e668f6a9
changxianglin <281532612@qq.com>
2020-08-04 23:58:43 +0800

    wirte api

--- blog-express/app.js
+++ blog-express/app.js
@@ -12,17 +12,17 @@ const userRouter = require('./routes/user')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter)
 app.use('/api/user', userRouter)
 
