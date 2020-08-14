7f84d28f0ba286f88ff78c61a827308274c1d908
changxianglin <281532612@qq.com>
2020-08-14 23:59:46 +0800

    list

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,14 +1,30 @@
 const router = require('koa-router')()
 
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SucessModel, ErrorModel } = require('../modal/resmodel')
+const loginCheck = require('../middleware/loginCheck')
+
 router.prefix('/api/blog')
 
 router.get('/list', async function (ctx, next) {
-  const query = ctx.query
-  ctx.body = {
-    errno: 0,
-    query,
-    data: ['获取博客列表']
+  let author = ctx.query.author || ''
+  const keyword = ctx.query.keyword || ''
+
+  if (ctx.query.isadmin) {
+      console.log('is admin')
+      // 管理员界面
+      if (ctx.session.username == null ) {
+          console.error('is admin, but no login')
+          // 未登录
+          ctx.body = new ErrorModel('未登录')
+          return
+      }
+
+      // 强制查询自己的博客
+      author = ctx.session.username
   }
+  const listData = await getList(author, keyword)
+  ctx.body = new SucessModel(listData)
 })
 
 module.exports = router
